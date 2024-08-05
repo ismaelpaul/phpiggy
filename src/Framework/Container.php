bb67f06a260cfd41d2525269f5550608c7b8c583
ismaelpaul <ismaafp@gmail.com>
2024-08-05 19:32:10 +0100

    Update Container class to perform validations

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use ReflectionClass, ReflectionNamedType;
+use Framework\Exceptions\ContainerException;
+
 class Container
 {
     private array $definitions = [];
@@ -11,6 +14,43 @@ class Container
     public function addDefinitions(array $newDefinitions)
     {
         $this->definitions = [...$this->definitions, ...$newDefinitions];
-        dd($this->definitions);
+    }
+
+    public function resolve(string $className)
+    {
+        $reflectionClass = new ReflectionClass($className);
+
+        if (!$reflectionClass->isInstantiable()) {
+            throw new ContainerException("Class {$className} is not instantiable ");
+        }
+
+        $constructor = $reflectionClass->getConstructor();
+
+        if (!$constructor) {
+            return new $className;
+        }
+
+        $params = $constructor->getParameters();
+
+        if (count($params) === 0) {
+            return new $className;
+        }
+
+        $dependencies = [];
+
+        foreach ($params as $param) {
+            $name = $param->getName();
+            $type = $param->getType();
+
+            if (!$type) {
+                throw new ContainerException("Failed to resolve class {$className} because param {$name} is missing a type hint.");
+            }
+
+            if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+                throw new ContainerException("Failed to resolve class {$className} because invalid param name");
+            }
+        }
+
+        dd($params);
     }
 }
