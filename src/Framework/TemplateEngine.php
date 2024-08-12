5ee6bbab01c4fec35a61193eba8ced6876a6c76b
ismaelpaul <ismaafp@gmail.com>
2024-08-12 19:54:50 +0100

    Add addGlobal method

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -6,13 +6,13 @@ namespace Framework;
 
 class TemplateEngine
 {
-    public function __construct(private string $basePath)
-    {
-    }
+    private array $globalTemplateData = [];
+    public function __construct(private string $basePath) {}
 
     public function render(string $template, array $data = [])
     {
         extract($data, EXTR_SKIP);
+        extract($this->globalTemplateData, EXTR_SKIP);
 
         ob_start();
 
@@ -28,5 +28,10 @@ class TemplateEngine
     public function resolve(string $path)
     {
         return "{$this->basePath}/{$path}";
+    }
+
+    public function addGlobal(string $key, mixed $value)
+    {
+        $this->globalTemplateData[$key] = $value;
     }
 }
