8ed2e8491066dcc2e2ed0fb3dc57a3211b70131a
ismaelpaul <ismaafp@gmail.com>
2024-08-27 18:24:40 +0100

    Add oldFormData to flash middleware

--- src/App/Middleware/FlashMiddleware.php
+++ src/App/Middleware/FlashMiddleware.php
@@ -14,6 +14,13 @@ class FlashMiddleware implements MiddlewareInterface
     public function process(callable $next)
     {
         $this->view->addGlobal('errors', $_SESSION['errors'] ?? []);
+
+        unset($_SESSION['errors']);
+
+        $this->view->addGlobal('oldFormData', $_SESSION['oldFormData'] ?? []);
+
+        unset($_SESSION['oldFormData']);
+
         $next();
     }
 }
