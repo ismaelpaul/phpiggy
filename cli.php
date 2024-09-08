1109e83ecc02c1dbf855e49de9ff7742c58bbbe9
ismaelpaul <ismaafp@gmail.com>
2024-09-08 11:16:23 +0100

    Add db connection

---
+++ cli.php
@@ -0,0 +1,9 @@
+<?php
+
+include __DIR__ . '/src/Framework/Database.php';
+
+use Framework\Database;
+
+$db = new Database("mysql", ["host" => "127.0.0.1", "port" => "3306", "dbname" => "phpiggy"], "root", "");
+
+echo "Connected to database.";
