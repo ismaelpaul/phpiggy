0de70d07c7ee02216800fefae0b14b51f1a37a89
ismaelpaul <ismaafp@gmail.com>
2024-09-09 18:56:14 +0100

    Add query method

--- src/Framework/Database.php
+++ src/Framework/Database.php
@@ -8,7 +8,7 @@ use PDO, PDOException;
 
 class Database
 {
-    public PDO $connection;
+    private PDO $connection;
 
     public function __construct(string $driver, array $config, string $username, string $password)
     {
@@ -22,5 +22,10 @@ class Database
         } catch (PDOException $e) {
             die("Unable to connect to database!");
         }
+    }
+
+    public function query(string $query)
+    {
+        $this->connection->query($query);
     }
 }
