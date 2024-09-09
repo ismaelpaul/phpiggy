bb0d19b27100209bbf1347624d23a43cf4274e37
ismaelpaul <ismaafp@gmail.com>
2024-09-09 18:55:51 +0100

    Add $sqlFile variable and then execute query from the file

--- cli.php
+++ cli.php
@@ -6,4 +6,6 @@ use Framework\Database;
 
 $db = new Database("mysql", ["host" => "127.0.0.1", "port" => "3306", "dbname" => "phpiggy"], "root", "");
 
-echo "Connected to database.";
+$sqlFile = file_get_contents("./database.sql");
+
+$db->query($sqlFile);
