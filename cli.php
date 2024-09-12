ebc38c8c0a961d864190e225f9f6f73189cf4eaf
ismaelpaul <ismaafp@gmail.com>
2024-09-12 19:30:43 +0100

    Import environment variables

--- cli.php
+++ cli.php
@@ -2,9 +2,16 @@
 
 include __DIR__ . '/src/Framework/Database.php';
 
-use Framework\Database;
+require __DIR__ . "/../../vendor/autoload.php";
 
-$db = new Database("mysql", ["host" => "127.0.0.1", "port" => "3306", "dbname" => "phpiggy"], "root", "");
+use Framework\Database;
+use Dotenv\Dotenv;
+use App\Config\Paths;
+
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
+
+$db = new Database($_ENV['DB_DRIVER'], ['host' => $_ENV['DB_HOST'], 'port' => $_ENV['DB_PORT'], 'dbname' => $_ENV['DB_NAME']], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 
 $sqlFile = file_get_contents("./database.sql");
 
