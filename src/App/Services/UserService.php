417b112a43358b82cbc4fa4eff877128031eb205
ismaelpaul <ismaafp@gmail.com>
2024-09-15 10:34:02 +0100

    Add isEmailTaken and create methods

--- src/App/Services/UserService.php
+++ src/App/Services/UserService.php
@@ -5,8 +5,40 @@ declare(strict_types=1);
 namespace App\Services;
 
 use Framework\Database;
+use Framework\Exceptions\ValidationException;
 
 class UserService
 {
     public function __construct(private Database $db) {}
+
+    public function isEmailTaken(string $email)
+    {
+        $emailCount = $this->db->query(
+            "SELECT COUNT(*) FROM users WHERE email = :email",
+            ['email' => $email]
+        )->count();
+
+        if ($emailCount > 0) {
+            throw new ValidationException(['email' => 'Email already registered.']);
+        }
+    }
+
+    public function create(array $formData)
+    {
+
+        $password = password_hash($formData['password'], PASSWORD_BCRYPT, ['cost' => 12]);
+
+
+        $this->db->query(
+            "INSERT INTO users(email, password, age, country, social_media_url)
+            VALUES(:email, :password, :age, :country, :url)",
+            [
+                'email' => $formData['email'],
+                'password' => $password,
+                'age' => $formData['age'],
+                'country' => $formData['country'],
+                'url' => $formData['socialMediaUrl']
+            ]
+        );
+    }
 }
