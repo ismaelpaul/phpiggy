af43904ad18389e50c69f12e9383c173e1c7a7cc
ismaelpaul <ismaafp@gmail.com>
2024-09-17 19:50:30 +0100

    Add logout method

--- src/App/Services/UserService.php
+++ src/App/Services/UserService.php
@@ -40,6 +40,10 @@ class UserService
                 'url' => $formData['socialMediaUrl']
             ]
         );
+
+        session_regenerate_id();
+
+        $_SESSION['user'] = $this->db->id();
     }
 
     public function login(array $formData)
@@ -58,5 +62,12 @@ class UserService
         session_regenerate_id();
 
         $_SESSION['user'] = $user['id'];
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['user']);
+
+        session_regenerate_id();
     }
 }
