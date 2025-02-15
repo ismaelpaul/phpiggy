4983e8fe2e4a5176b2592cccba76b7da1167e7fc
ismaelpaul <ismaafp@gmail.com>
2025-02-15 10:52:37 +0000

    Add login and logout methods

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -35,4 +35,20 @@ class AuthController
   {
     echo $this->view->render("login.php");
   }
+
+  public function login()
+  {
+    $this->validatorService->validateLogin($_POST);
+
+    $this->userService->login($_POST);
+
+    redirectTo('/');
+  }
+
+  public function logout()
+  {
+    $this->userService->logout();
+
+    redirectTo('/login');
+  }
 }
