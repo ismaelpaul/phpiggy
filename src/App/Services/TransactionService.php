37598d4ab0add43fc67318aa3968ef75bfe17ce6
ismaelpaul <ismaafp@gmail.com>
2025-02-20 19:28:31 +0000

    Add TransactionService file

---
+++ src/App/Services/TransactionService.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use Framework\Database;
+
+class TransactionService
+{
+    public function __construct(private Database $db) {}
+
+    public function create(array $formData)
+    {
+        $formattedDate = "{$formData['date']} 00:00:00";
+
+        $this->db->query("
+        INSERT INTO transactions(user_id, description, amount, date)
+        VALUES(:user_id, :description, :amount, :date)
+        ", [
+            'user_id' => $_SESSION['user'],
+            'description' => $formData['description'],
+            'amount' => $formData['amount'],
+            'date' => $formattedDate
+        ]);
+    }
+
+    public function getUserTransactions()
+    {
+        $searchTerm = addcslashes($_GET['s'] ?? '', '%_');
+
+        $transactions = $this->db->query("
+        SELECT *, DATE_FORMAT(date, '%D %M, %Y') as formatted_date
+        FROM transactions WHERE user_id = :user_id
+        AND description LIKE :description
+        ", [
+            'user_id' => $_SESSION['user'],
+            'description' => "%{$searchTerm}%"
+        ])->findAll();
+
+        return $transactions;
+    }
+}
