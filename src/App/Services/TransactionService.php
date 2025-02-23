cb4b8b63a0463a730378c948ef80cefe5431bd94
ismaelpaul <ismaafp@gmail.com>
2025-02-23 17:32:24 +0000

    Update getUserTransactions to render pagination

--- src/App/Services/TransactionService.php
+++ src/App/Services/TransactionService.php
@@ -25,19 +25,77 @@ class TransactionService
         ]);
     }
 
-    public function getUserTransactions()
+    public function getUserTransactions(int $length, int $offset)
     {
         $searchTerm = addcslashes($_GET['s'] ?? '', '%_');
-
-        $transactions = $this->db->query("
-        SELECT *, DATE_FORMAT(date, '%D %M, %Y') as formatted_date
-        FROM transactions WHERE user_id = :user_id
-        AND description LIKE :description
-        ", [
+        $params = [
             'user_id' => $_SESSION['user'],
             'description' => "%{$searchTerm}%"
-        ])->findAll();
+        ];
 
-        return $transactions;
+        $transactions = $this->db->query(
+            "SELECT *, DATE_FORMAT(date, '%D %M, %Y') as formatted_date
+        FROM transactions 
+        WHERE user_id = :user_id
+        AND description LIKE :description
+        LIMIT {$length} OFFSET {$offset}",
+            $params
+        )->findAll();
+
+        $transactionCount = $this->db->query(
+            "SELECT COUNT(*)
+        FROM transactions 
+        WHERE user_id = :user_id
+        AND description LIKE :description",
+            $params
+
+        )->count();
+
+        return [$transactions, $transactionCount];
+    }
+
+    public function getUserTransaction(string $id)
+    {
+        return $this->db->query(
+            "SELECT *, DATE_FORMAT(date, '%Y-%m-%d') as formatted_date
+            FROM transactions 
+            WHERE id = :id AND user_id = :user_id",
+            [
+                'id' => $id,
+                'user_id' => $_SESSION['user']
+            ]
+        )->find();
+    }
+
+    public function update(array $formData, int $id)
+    {
+        $formattedDate = "{$formData['date']} 00:00:00";
+
+        $this->db->query(
+            "UPDATE transactions
+            SET description = :description,
+              amount = :amount,
+              date = :date
+            WHERE id = :id
+            AND user_id = :user_id",
+            [
+                'description' => $formData['description'],
+                'amount' => $formData['amount'],
+                'date' => $formattedDate,
+                'id' => $id,
+                'user_id' => $_SESSION['user']
+            ]
+        );
+    }
+
+    public function delete(int $id)
+    {
+        $this->db->query(
+            "DELETE FROM transactions WHERE id = :id AND user_id = :user_id",
+            [
+                'id' => $id,
+                'user_id' => $_SESSION['user']
+            ]
+        );
     }
 }
